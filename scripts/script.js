c1a2e871ee1e26b6da857208c637f4fcd7023145
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-04-29 18:20:26 +0200

    develop(homepage)

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 
+let backToTopBtn = document.querySelector("#backToTopBtn")
+
+window.onscroll = () => {scrollFunction()}
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        // backToTopBtn.classList.add("slide-back")
+        backToTopBtn.style.display = "block"
+    }
+    else{
+        backToTopBtn.style.display = "none"
+        // backToTopBtn.classList.remove("slide-back")
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
